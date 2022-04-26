70bd210950dbb355324e68ca3a392a7bbcb4f1e0
Gladys Mwangi <gladysmwangi04@yahoo.com>
2022-04-26 15:21:33 +0300

    implementing bootstrap

---
+++ app/movie_test.py
@@ -0,0 +1,21 @@
+from re import M
+import unittest
+from models import movie
+Movie = movie.Movie
+
+class MovieTest(unittest.TestCase):
+    """
+    test class to test the behavior of the movie class
+    """
+
+    def setUp(self):
+        """
+        Set up method that will run before every Test  
+        """
+        self.new_movie = Movie(1234, 'Python Must Be Crazy', 'A thrilling new Python Series', 'https://image.tmbd.org/t/p/w500/khsjha27hbs', 8.5,129993)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_movie,  Movie))
+
+if __name__ =='__main__':
+    unittest.main()
---
+++ app/views.py
@@ -0,0 +1,30 @@
+
+from turtle import title
+from flask import render_template
+from  app import app
+from .request import get_movies
+
+# Views
+@app.route('/')
+def index():
+    
+    '''
+    View root page function that returns the index page and its data
+    '''
+
+    title = 'Home - Welcome to the best movie review website'
+    return render_template('index.html',title = title)
+
+
+@app.route('/movie/<movie_id>')
+def movie(movie_id):
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+    popular_movies = get_movies('popular')
+    upcoming_movie = get_movies('upcoming')
+    now_showing_movie = get_movies('now_playing')
+
+    title = f'You are viewing {movie_id}'
+    return render_template('movie.html',title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie)
