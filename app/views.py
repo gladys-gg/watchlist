621b97eca4bcfa1ccd426c35e5f5947395ca581e
Gladys Mwangi <gladysmwangi04@yahoo.com>
2022-04-27 19:13:39 +0300

    implemented the wtf forms

--- app/views.py
+++ app/views.py
@@ -3,6 +3,11 @@ from turtle import title
 from flask import render_template, request, redirect,url_for
 from  app import app
 from .request import get_movies, get_movie, search_movie
+from .models import review
+from .forms import ReviewForm
+Review = review.Review
+
+
 
 # Views
 @app.route('/')
@@ -25,7 +30,7 @@ def index():
 
 
 @app.route('/movie/<int:id>')
-def movie(movie_id):
+def movie(id):
 
     '''
     View root page function that returns the index page and its data
@@ -33,7 +38,8 @@ def movie(movie_id):
 
     movie = get_movie(id)
     title = f'You are viewing {movie.title}'
-    return render_template('movie.html',title = title, movie = movie)
+    reviews = Review.get_reviews(movie.id)
+    return render_template('movie.html',title = title, movie = movie, reviews = reviews)
 
 @app.route('/search/<movie_name>')
 def search(movie_name):
@@ -45,3 +51,19 @@ def search(movie_name):
     searched_movies = search_movie(movie_name_format)
     title = f'search results for {movie_name}'
     return render_template('search.html',movies = searched_movies)
+
+
+@app.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
+def new_review(id):
+    form = ReviewForm()
+    movie = get_movie(id)
+
+    if form.validate_on_submit():
+        title = form.title.data
+        review = form.review.data
+        new_review = Review(movie.id,title,movie.poster,review)
+        new_review.save_review()
+        return redirect(url_for('movie',id = movie.id ))
+
+    title = f'{movie.title} review'
+    return render_template('new_review.html',title = title, review_form=form, movie=movie)
